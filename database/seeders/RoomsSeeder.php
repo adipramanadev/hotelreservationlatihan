b9dfa48e9a2e6ddd5c1e7e7f379190c3f715610a
Adipramana <herryprasetyo.dev@gmail.com>
2025-01-20 08:35:32 +0700

    first commit

---
+++ database/seeders/RoomsSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoomsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+        $rooms = []; //array itu adalah beberapa  data yang akan diinputkan ke dalam tabel rooms
+        for ($i=1; $i <=100 ; $i++) { 
+            $rooms[] = [
+                //mengisi data yang akan diinputkan ke dalam tabel rooms
+                'name' => 'Room ' . $i,
+                'capacity' => rand(1, 5),
+                'price' => rand(500, 500), //harga dalam satuan besar
+                'status' => ['available', 'reserved'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+        }
+
+        //memasukkan data yang sudah diinputkan ke dalam tabel rooms
+        foreach($rooms as $room){
+            App\Models\Room::create($room); //menyimpan data ke dalam tabel rooms dengan meggunakan elequent
+        }
+    }
+}
